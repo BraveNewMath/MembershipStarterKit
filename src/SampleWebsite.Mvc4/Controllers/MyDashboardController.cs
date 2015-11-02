93663f8f2e3c09717cc4d90ba41268d85db9c213
elias <ehassen@esri.com>
2015-11-01 23:42:55 -0800

    Added mydashboard - sample secure section

---
+++ src/SampleWebsite.Mvc4/Controllers/MyDashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace SampleWebsite.Mvc4.Controllers
+{
+    [Authorize]
+	public class MyDashboardController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "This is your dashboard";
+
+			return View();
+		}
+        
+	}
+}
